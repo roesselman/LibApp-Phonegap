0d7823ff56dffb4b77d48c672b7c86709a07b54f
roesselman <maichelvanroessel@MacBook-Pro-van-Maichel-van-Roessel.local>
2013-10-08 12:57:29 +0200

    Added alerts to do debug

--- LibApp-Phonegap/www/js/index.js
+++ LibApp-Phonegap/www/js/index.js
@@ -119,16 +119,19 @@ function initApp(){
     }
 
     function querySuccess(tx, results) {
+        alert("Query trys to work.");
+
         console.log("Returned rows = " + results.rows.length);
         // this will be true since it was a select statement and so rowsAffected was 0
         if (!results.rowsAffected) {
             console.log('No rows affected!');
+            alert("No rows affected.");
             return false;
         }
 
         // for an insert statement, this property will return the ID of the last inserted row
         console.log("Last inserted row ID = " + results.insertId);
-
+        alert("Trying to read results");
         var len = results.rows.length;
 
         for (var i=0; i<len; i++){
